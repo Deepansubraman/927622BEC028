64383e3371833a9b63da480c12eaf8c10f34ebfa
Deepansubraman <deepandeepan516@gmail.com>
2025-05-24 12:25:35 +0530

    Initial commit with both folders

--- mkdir-Question1/src/App.jsx
+++ mkdir-Question1/src/App.jsx
@@ -1,35 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import { getAuthToken, getStocks } from "./api";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [token, setToken] = useState("");
+  const [stocks, setStocks] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    async function fetchTokenAndStocks() {
+      setLoading(true);
+      setError("");
+      try {
+        const tok = await getAuthToken();
+        setToken(tok);
+        const stocksList = await getStocks(tok);
+        setStocks(stocksList);
+      } catch (err) {
+        setError(err.message);
+        console.error(err);
+      }
+      setLoading(false);
+    }
+    fetchTokenAndStocks();
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <h1>Token:</h1>
+      <pre>{token}</pre>
+      <h2>Stocks:</h2>
+      {error && <p style={{ color: "red" }}>Error: {error}</p>}
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <ul>
+          {stocks.map((stock) => (
+            <li key={stock.symbol}>
+              {stock.companyName} (<b>{stock.symbol}</b>)
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
 }
 
-export default App
+export default App;
--- mkdir-Question1/vite.config.js
+++ mkdir-Question1/vite.config.js
@@ -1,10 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000
+    port: 3000,
+    proxy: {
+      '/token': 'http://20.244.56.144',
+      '/evaluation-service': 'http://20.244.56.144',
+      '/stocks': 'http://20.244.56.144'
+    }
   }
 })
